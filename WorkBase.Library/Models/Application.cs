06ed5580f384a5100b4eb67fcb467c1ba3c2b69e
Oteo Mamo <oteomamo@gmail.com>
2023-09-10 22:22:05 -0400

    Services Set Up Utilities Set Up

--- WorkBase.Library/Models/Application.cs
+++ WorkBase.Library/Models/Application.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace WorkBase.Library.Models
 {
@@ -14,15 +15,21 @@ namespace WorkBase.Library.Models
         public DateTime DateApplied { get; set; }
         public string ApplicationStatus { get; set; }
         public string JobDescription { get; set; }
-    }
 
-    public Application()
-    {
-        ID = 0;
-        EmployerName = string.Empty;
-        JobTitle = string.Empty;
-        DateApplied = DateTime.Now;
-        ApplicationStatus = string.Empty;
-        JobDescription = string.Empty;
+
+        public Application()
+        {
+            Id = 0;
+            EmployerName = string.Empty;
+            JobTitle = string.Empty;
+            DateApplied = DateTime.Now;
+            ApplicationStatus = string.Empty;
+            JobDescription = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(" Id: {0,-3}\tEmployerName: {1,-20}\tJobTitle: {2,-20}\tDateApplied: {3,-10:MM/dd/yyyy}\tApplicationStatus: {4,-5}\tJobDescription: {5,-30}", Id, EmployerName, JobTitle, DateApplied, ApplicationStatus, JobDescription);
+        }
     }
 }
---
+++ WorkBase.Library/Services/UserServices.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkBase.Library.Models;
+
+namespace WorkBase.Library.Services
+{
+    public class UserServices
+    {
+        private static UserServices? instance;
+
+        public static UserServices Current
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new UserServices();
+                }
+                return instance;
+            }
+        }
+
+        private List<User> users;
+
+        public List<User> Users
+        {
+            get
+            {
+                return users;
+            }
+        }
+
+        private UserServices()
+        {
+            users = new List<User>
+            {
+                   new User { Id = 1, Name = "John Doe", EmailAddress = "jd@gmail.com", ActiveApplication = 1, TotalApplications = 2, Password = "1234" },
+                   new User { Id = 1, Name = "Bob Smith", EmailAddress = "bs@gmail.com", ActiveApplication = 1, TotalApplications = 2, Password = "1234" },
+                   new User { Id = 1, Name = "Ana Smith", EmailAddress = "as@gmail.com", ActiveApplication = 1, TotalApplications = 2, Password = "1234" }
+            };
+        }
+
+        public User? GetUser(string emailAddress, string password)
+        {
+            User? user = null;
+
+            foreach (User u in users)
+            {
+                if (u.EmailAddress == emailAddress && u.Password == password)
+                {
+                    user = u;
+                    break;
+                }
+            }
+
+            return user;
+        }
+
+
+
+    }
+}
