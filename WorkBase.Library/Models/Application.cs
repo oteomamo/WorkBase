58d56e4114cc1c0ad9f8e2573a9c610a3d03bc1f
Oteo Mamo <oteomamo@gmail.com>
2023-09-10 21:50:59 -0400

    Add project files.

---
+++ WorkBase.Library/Models/Application.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkBase.Library.Models
+{
+    public class Application
+    {
+        public int Id { get; set; }
+        public string EmployerName { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime DateApplied { get; set; }
+        public string ApplicationStatus { get; set; }
+        public string JobDescription { get; set; }
+    }
+
+    public Application()
+    {
+        ID = 0;
+        EmployerName = string.Empty;
+        JobTitle = string.Empty;
+        DateApplied = DateTime.Now;
+        ApplicationStatus = string.Empty;
+        JobDescription = string.Empty;
+    }
+}
---
+++ WorkBase.Library/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkBase.Library.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
+
+        public int TotalApplications { get; set; }
+        public int ActiveApplication { get; set; }
+
+        public List<Application>? Applications { get; set; }
+
+
+        public User()
+        {
+            Id = 0;
+            Name = string.Empty;
+            EmailAddress = string.Empty;
+            Password = string.Empty;
+            TotalApplications = 0;
+            ActiveApplication = 0;
+            Applications = new List<Application>();
+        }
+
+
+
+    }
+}
