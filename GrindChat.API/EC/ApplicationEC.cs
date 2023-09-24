94165ce35dfbd43577c974d5e3aa76c1adb6ba08
oteomamo <oteomamo@gmail.com>
2023-09-23 21:59:37 -0400

    API EC Implementation

---
+++ GrindChat.API/EC/ApplicationEC.cs
@@ -0,0 +1,40 @@
+﻿using WorkBase.API.Database;
+using WorkBase.Library.DTO;
+using WorkBase.Library.Models;
+
+namespace WorkBase.API.EC
+{
+    public class ApplicationEC
+    {
+        public ApplicationDTO? Get(int id)
+        {
+            var application = Filebase.Current.Applications.FirstOrDefault(u => u.Id == id);
+            return application != null ? new ApplicationDTO(application) : null;
+        }
+
+        public Application AddOrUpdate(Application application)
+        {
+            return Filebase.Current.AddOrUpdate(application);
+        }
+
+        public ApplicationDTO? Delete(int id)
+        {
+            var applicationToDelete = Filebase.Current.Applications.FirstOrDefault(c => c.Id == id);
+            if (applicationToDelete != null)
+            {
+                Filebase.Current.Applications.Remove(applicationToDelete);
+                Filebase.Current.DeleteApplication(id.ToString());
+            }
+            return applicationToDelete != null ? new ApplicationDTO(applicationToDelete) : null;
+        }
+
+        public IEnumerable<ApplicationDTO> Search(string query = "")
+        {
+            return Filebase.Current.Applications
+                .Where(c => c.Company.ToUpper()
+                    .Contains(query.ToUpper()))
+                .Take(1000)
+                .Select(c => new ApplicationDTO(c));
+        }
+    }
+}
---
+++ GrindChat.API/EC/UserEC.cs
@@ -0,0 +1,40 @@
+﻿using WorkBase.API.Database;
+using WorkBase.Library.DTO;
+using WorkBase.Library.Models;
+
+namespace WorkBase.API.EC
+{
+    public class UserEC
+        {
+            public UserDTO? Get(int id)
+            {
+                var user = Filebase.Current.Users.FirstOrDefault(u => u.Id == id);
+                return user != null ? new UserDTO(user) : null;
+            }
+
+            public User AddOrUpdate(User user)
+            {
+                return Filebase.Current.AddOrUpdate(user);
+            }
+
+            public UserDTO? Delete(int id)
+            {
+                var userToDelete = Filebase.Current.Users.FirstOrDefault(c => c.Id == id);
+                if (userToDelete != null)
+                {
+                    Filebase.Current.Users.Remove(userToDelete);
+                    Filebase.Current.DeleteUser(id.ToString());
+                }
+                return userToDelete != null ? new UserDTO(userToDelete) : null;
+            }
+
+            public IEnumerable<UserDTO> Search(string query = "")
+            {
+                return Filebase.Current.Users
+                    .Where(c => c.Name.ToUpper()
+                        .Contains(query.ToUpper()))
+                    .Take(1000)
+                    .Select(c => new UserDTO(c));
+            }
+        }
+    }
